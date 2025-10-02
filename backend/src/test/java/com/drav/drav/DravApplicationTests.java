1f6295fa28e4b7069625ba28e0b26f850d4d4352
Manrick-Lazaro <manricksantos@gmail.com>
2025-10-02 16:14:45 -0300

    feat: initiate a backend project

---
+++ backend/src/test/java/com/drav/drav/DravApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drav.drav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DravApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
