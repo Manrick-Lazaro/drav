bf5c1654ce3d2c5c287c0466cb60acec9506eb4b
Manrick-Lazaro <manricksantos@gmail.com>
2025-10-03 10:12:59 -0300

    feat: create user model

---
+++ backend/src/main/java/com/drav/drav/models/User.java
@@ -0,0 +1,52 @@
+package com.drav.drav.models;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = User.TABLE_NAME)
+public class User {
+    public static final String TABLE_NAME = "user";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @JsonProperty(access = Access.WRITE_ONLY)
+    @Size(min = 8, max = 32)
+    @NotBlank(message = "A senha é obrigatória")
+    @Column(name = "password", nullable = false, length = 32)
+    private String password;
+
+    @Column(name = "last_login")
+    private LocalDateTime last_login;
+
+    @CreatedDate
+    @Column(name = "creation_date", updatable = false, nullable = false)
+    private LocalDateTime creation_date;
+
+    @NotBlank(message = "O nome é obrigatório")
+    @Size(min = 2, max = 100)
+    @Column(name = "name", length = 100, unique = true, nullable = false)
+    private String name;
+
+    @NotBlank(message = "O e-mail é obrigatório")
+    @Email(message = "E-mail inválido")
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+}
